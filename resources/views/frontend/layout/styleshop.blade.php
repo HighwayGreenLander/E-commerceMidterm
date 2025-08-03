4507f78aa6b02ed3d3e001bfd8225b30cfb4d5b1
david <david4rtz@gmail.com>
2025-08-03 21:16:44 +0700

    D

--- resources/views/frontend/layout/styleshop.blade.php
+++ resources/views/frontend/layout/styleshop.blade.php
@@ -1,32 +1,30 @@
 <!-- <link href="https://fonts.googleapis.com/css?family=Droid+Sans" rel="stylesheet"> -->
-	
-	<!-- Animate.css -->
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/animate.css')}}">
-	<!-- Icomoon Icon Fonts-->
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/icomoon.css')}}">
-	<!-- Themify Icons-->
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/themify-icons.css')}}">
-	<!-- Bootstrap  -->
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/bootstrap.css')}}">
 
-	<!-- Font Awesome CDN -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- Animate.css -->
+<link rel="stylesheet" href="{{ secure_asset('frontend/assets/css/animate.css') }}">
+<!-- Icomoon Icon Fonts-->
+<link rel="stylesheet" href="{{ secure_asset('frontend/assets/css/icomoon.css') }}">
+<!-- Themify Icons-->
+<link rel="stylesheet" href="{{ secure_asset('frontend/assets/css/themify-icons.css') }}">
+<!-- Bootstrap  -->
+<link rel="stylesheet" href="{{ secure_asset('frontend/assets/css/bootstrap.css') }}">
 
+<!-- Font Awesome CDN (already HTTPS, leave as is) -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-..." crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-	<!-- Magnific Popup -->
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/magnific-popup.css')}}">
+<!-- Magnific Popup -->
+<link rel="stylesheet" href="{{ secure_asset('frontend/assets/css/magnific-popup.css') }}">
 
-	<!-- Owl Carousel  -->
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/owl.carousel.min.css')}}">
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/owl.theme.default.min.css')}}">
+<!-- Owl Carousel  -->
+<link rel="stylesheet" href="{{ secure_asset('frontend/assets/css/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ secure_asset('frontend/assets/css/owl.theme.default.min.css') }}">
 
-	<!-- Theme style  -->
-	<link rel="stylesheet" href="{{asset('frontend/assets/css/style.css')}}">
+<!-- Theme style  -->
+<link rel="stylesheet" href="{{ secure_asset('frontend/assets/css/style.css') }}">
 
-	<!-- Modernizr JS -->
-	<script src="{{asset('frontend/assets/js/modernizr-2.6.2.min.js')}}"></script>
-	<!-- FOR IE9 below -->
-	<!--[if lt IE 9]>
-	<script src="js/respond.min.js"></script>
-	<![endif]-->
-
+<!-- Modernizr JS -->
+<script src="{{ secure_asset('frontend/assets/js/modernizr-2.6.2.min.js') }}"></script>
+<!-- FOR IE9 below -->
+<!--[if lt IE 9]>
+<script src="{{ secure_asset('frontend/assets/js/respond.min.js') }}"></script>
+<![endif]-->
